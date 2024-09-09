54f797c5e6941cdec125c9c96c430ae2dd372faa
monire91 <aglida1370@gmail.com>
2024-09-09 10:44:50 +0300

    moved map component to a parent component, to separate functionalities and keep small components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import Map from "./components/Map";
+import Map from "./components/map/index";
+import Search from "./components/Search";
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
+      <div className="relative h-screen">
+        <Search />
         <Map />
       </div>
     </Provider>
---
+++ src/components/map/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import RecenterMap from "./RecenterMap";
+import L from "leaflet"; // Import Leaflet
+
+// Import the default marker images
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import markerRetina from "leaflet/dist/images/marker-icon-2x.png";
+import ControlPanel from "./ControlPanel";
+
+// Fix the marker icon issue by setting the default icon
+const DefaultIcon = L.icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerRetina,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon; // Set this as the default
+
+// Define the type for the tile layer keys
+type TileLayerType = "light" | "dark" | "satellite";
+
+const Map: React.FC = () => {
+  const { coordinates, name, country, population } = useSelector(
+    (state: RootState) => state.city
+  );
+
+  const [tileLayer, setTileLayer] = useState<TileLayerType>("light");
+
+  const tileLayers: Record<TileLayerType, string> = {
+    light: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    dark: "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+    satellite:
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+  };
+
+  return (
+    <div className="relative">
+      <MapContainer
+        center={coordinates || [51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+        zoomControl={true}
+        className="h-screen"
+      >
+        <TileLayer
+          url={tileLayers[tileLayer]}
+          attribution="Karun Test Project by Ali Najdi"
+          zIndex={1}
+        />
+        {coordinates && (
+          <>
+            <Marker position={[coordinates.lat, coordinates.lng]}>
+              <Popup>
+                {name}, {country}
+                {population && <div>Population: {population}</div>}
+              </Popup>
+            </Marker>
+            <RecenterMap coordinates={coordinates} />
+          </>
+        )}
+      </MapContainer>
+
+      {/* Theme Switch Buttons */}
+      <ControlPanel tileLayer={tileLayer} setTileLayer={setTileLayer} />
+    </div>
+  );
+};
+
+export default Map;
